d08a1da4af863117a6693e5974e7ff59134cec74
Jacob Feng <peanutchowder@gmail.com>
2025-10-16 00:02:15 -0600

    Removed duplicate constructor declarations for nodes

--- src/AST.cpp
+++ src/AST.cpp
@@ -1,26 +1,28 @@
 #include "AST.h"
-#include <vector>
-#include <string>
-#include <memory>
-#include "AST.h"
 #include "ASTVisitor.h"
 
-// ASTNode base implementation 
-ASTNode::~ASTNode() = default;
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
 
-// ExprNode
-ExprNode::ExprNode() { type = ValueType::UNKNOWN; }
-ExprNode::~ExprNode() = default;
+ExprNode::ExprNode() {
+    type = ValueType::UNKNOWN;
+}
 
 // IntNode
-IntNode::IntNode(int v) : value(v) { type = ValueType::INTEGER; }
+IntNode::IntNode(int v) : value(v) {
+    type = ValueType::INTEGER;
+}
 std::string IntNode::toString() const {
     return std::to_string(value);
 }
 void IntNode::accept(ASTVisitor& visitor) { visitor.visit(this); }
 
 // IdNode
-IdNode::IdNode(const std::string& i) : name(i) { type = ValueType::UNKNOWN; }
+IdNode::IdNode(const std::string& i) : name(i) {
+    type = ValueType::UNKNOWN;
+}
 std::string IdNode::toString() const {
     return name;
 }
@@ -28,7 +30,9 @@ void IdNode::accept(ASTVisitor& visitor) { visitor.visit(this); }
 
 // BinaryOpNode
 BinaryOpNode::BinaryOpNode(std::shared_ptr<ExprNode> l, std::shared_ptr<ExprNode> r, const std::string& o)
-    : left(std::move(l)), right(std::move(r)), op(o) { type = ValueType::UNKNOWN; }
+    : left(std::move(l)), right(std::move(r)), op(o) {
+    type = ValueType::UNKNOWN;
+}
 std::string BinaryOpNode::toString() const {
     return left->toString() + " " + op + " " + right->toString();
 }
@@ -36,7 +40,9 @@ void BinaryOpNode::accept(ASTVisitor& visitor) { visitor.visit(this); }
 
 // RangeNode
 RangeNode::RangeNode(std::shared_ptr<ExprNode> s, std::shared_ptr<ExprNode> e)
-    : start(std::move(s)), end(std::move(e)) { type = ValueType::VECTOR; }
+    : start(std::move(s)), end(std::move(e)) {
+    type = ValueType::VECTOR;
+}
 std::string RangeNode::toString() const {
     return start->toString() + " .. " + end->toString();
 }
@@ -44,7 +50,9 @@ void RangeNode::accept(ASTVisitor& visitor) { visitor.visit(this); }
 
 // IndexNode
 IndexNode::IndexNode(std::shared_ptr<ExprNode> a, std::shared_ptr<ExprNode> i)
-    : array(std::move(a)), index(std::move(i)) { type = ValueType::UNKNOWN; }
+    : array(std::move(a)), index(std::move(i)) {
+    type = ValueType::UNKNOWN;
+}
 std::string IndexNode::toString() const {
     return array->toString() + "[" + index->toString() + "]";
 }
@@ -52,7 +60,9 @@ void IndexNode::accept(ASTVisitor& visitor) { visitor.visit(this); }
 
 // GeneratorNode
 GeneratorNode::GeneratorNode(std::shared_ptr<IdNode> i, std::shared_ptr<ExprNode> d, std::shared_ptr<ExprNode> b)
-    : id(i), domain(std::move(d)), body(std::move(b)) { type = ValueType::VECTOR; }
+    : id(std::move(i)), domain(std::move(d)), body(std::move(b)) {
+    type = ValueType::VECTOR;
+}
 std::string GeneratorNode::toString() const {
     return "[ " + id->name + " in " + domain->toString() + " | " + body->toString() + " ]";
 }
@@ -60,7 +70,9 @@ void GeneratorNode::accept(ASTVisitor& visitor) { visitor.visit(this); }
 
 // FilterNode
 FilterNode::FilterNode(std::shared_ptr<IdNode> i, std::shared_ptr<ExprNode> d, std::shared_ptr<ExprNode> p)
-    : id(i), domain(std::move(d)), predicate(std::move(p)) { type = ValueType::VECTOR; }
+    : id(std::move(i)), domain(std::move(d)), predicate(std::move(p)) {
+    type = ValueType::VECTOR;
+}
 std::string FilterNode::toString() const {
     return "[ " + id->name + " in " + domain->toString() + " & " + predicate->toString() + " ]";
 }
@@ -68,7 +80,9 @@ void FilterNode::accept(ASTVisitor& visitor) { visitor.visit(this); }
 
 // IntDecNode
 IntDecNode::IntDecNode(std::shared_ptr<IdNode> i, std::shared_ptr<ExprNode> v)
-    : id(i), value(std::move(v)) { type = ValueType::INTEGER; }
+    : id(std::move(i)), value(std::move(v)) {
+    type = ValueType::INTEGER;
+}
 std::string IntDecNode::toString() const {
     return "int " + id->name + " = " + value->toString();
 }
@@ -76,7 +90,9 @@ void IntDecNode::accept(ASTVisitor& visitor) { visitor.visit(this); }
 
 // VectorDecNode
 VectorDecNode::VectorDecNode(std::shared_ptr<IdNode> i, std::shared_ptr<ExprNode> v)
-    : id(i), vectorValue(std::move(v)) { type = ValueType::VECTOR; }
+    : id(std::move(i)), vectorValue(std::move(v)) {
+    type = ValueType::VECTOR;
+}
 std::string VectorDecNode::toString() const {
     return "vector " + id->name + " = " + vectorValue->toString();
 }
@@ -84,7 +100,9 @@ void VectorDecNode::accept(ASTVisitor& visitor) { visitor.visit(this); }
 
 // AssignNode
 AssignNode::AssignNode(std::shared_ptr<IdNode> i, std::shared_ptr<ExprNode> v)
-    : id(i), value(std::move(v)) { type = ValueType::UNKNOWN; }
+    : id(std::move(i)), value(std::move(v)) {
+    type = ValueType::UNKNOWN;
+}
 std::string AssignNode::toString() const {
     return id->name + " = " + value->toString();
 }
@@ -92,7 +110,9 @@ void AssignNode::accept(ASTVisitor& visitor) { visitor.visit(this); }
 
 // PrintNode
 PrintNode::PrintNode(std::shared_ptr<ExprNode> e)
-    : printExpr(std::move(e)) { type = ValueType::UNKNOWN; }
+    : printExpr(std::move(e)) {
+    type = ValueType::UNKNOWN;
+}
 std::string PrintNode::toString() const {
     return "print(" + printExpr->toString() + ")";
 }
@@ -100,7 +120,9 @@ void PrintNode::accept(ASTVisitor& visitor) { visitor.visit(this); }
 
 // CondNode
 CondNode::CondNode(std::shared_ptr<ExprNode> cond, std::vector<std::shared_ptr<ASTNode>> stmts)
-    : ifCond(std::move(cond)), body(std::move(stmts)) { type = ValueType::UNKNOWN; }
+    : ifCond(std::move(cond)), body(std::move(stmts)) {
+    type = ValueType::UNKNOWN;
+}
 std::string CondNode::toString() const {
     std::string result = "if (" + ifCond->toString() + ")\n";
     for (const auto& stmt : body) {
@@ -113,7 +135,9 @@ void CondNode::accept(ASTVisitor& visitor) { visitor.visit(this); }
 
 // LoopNode
 LoopNode::LoopNode(std::shared_ptr<ExprNode> cond, std::vector<std::shared_ptr<ASTNode>> stmts)
-    : loopCond(std::move(cond)), body(std::move(stmts)) { type = ValueType::UNKNOWN; }
+    : loopCond(std::move(cond)), body(std::move(stmts)) {
+    type = ValueType::UNKNOWN;
+}
 std::string LoopNode::toString() const {
     std::string result = "loop (" + loopCond->toString() + ")\n";
     for (const auto& stat : body) {
@@ -126,7 +150,9 @@ void LoopNode::accept(ASTVisitor& visitor) { visitor.visit(this); }
 
 // FileNode
 FileNode::FileNode(std::vector<std::shared_ptr<ASTNode>> stmts)
-    : statements(std::move(stmts)) { type = ValueType::UNKNOWN; }
+    : statements(std::move(stmts)) {
+    type = ValueType::UNKNOWN;
+}
 std::string FileNode::toString() const {
     std::string result;
     for (const auto& stmt : statements) {
@@ -135,7 +161,6 @@ std::string FileNode::toString() const {
     return result;
 }
 void FileNode::accept(ASTVisitor& visitor) { visitor.visit(this); }
-
 
 
 
