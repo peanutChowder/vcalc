35e8bf0b9101f58b58a3b021ed2b78847d3d3686
Jacob Feng <peanutchowder@gmail.com>
2025-10-15 20:40:03 -0600

    Implemented semantic analysis for intDec, vecDec, assign, IdNode

--- src/SemanticAnalysisVisitor.cpp
+++ src/SemanticAnalysisVisitor.cpp
@@ -41,6 +41,55 @@ void SemanticAnalysisVisitor::visit(LoopNode* node) {
     exitScope();
 }
 
+void SemanticAnalysisVisitor::visit(IntDecNode* node) {
+    node->value->accept(*this);
+    node->id->accept(*this);
+
+    // Ensure type coersion is possible
+    if (node->value->type != ValueType::INTEGER) {
+        throw std::runtime_error("TypeError: Non-numeric type '" + toString(node->value->type) + "' cannot be declared to identifier of type int.");
+    }
+
+    current_->declare(node->id->name, ValueType::INTEGER);
+}
+
+void SemanticAnalysisVisitor::visit(VectorDecNode* node) {
+    node->vectorValue->accept(*this);
+    node->id->accept(*this);
+
+    // Ensure type coersion is possible
+    if (node->vectorValue->type != ValueType::VECTOR) {
+        throw std::runtime_error("TypeError: Non-vec type '" + toString(node->vectorValue->type) + "' cannot be declared to identifier of type vector.");
+    }
+
+    current_->declare(node->id->name, ValueType::VECTOR);
+}
+
+void SemanticAnalysisVisitor::visit(AssignNode* node) {
+    node->value->accept(*this);
+    node->id->accept(*this);
+
+    // Ensure type coersion is possible
+    if (promote(node->value->type, node->id->type) != node->id->type) {
+        throw std::runtime_error("TypeError: Value with type '" + toString(node->value->type) + "' cannot be assigned to identifier '" + node->id->name + "' with type '" + toString(node->id->type) + "'.");
+    }    
+}
+
+void SemanticAnalysisVisitor::visit(IntNode *node) {
+
+}
+
+void SemanticAnalysisVisitor::visit(IdNode *node) {
+    // Ensure var exists
+    SymbolInfo *idInfo = current_->resolve(node->name);
+    if (idInfo == nullptr) {
+        throw std::runtime_error("ReferenceError: Identifier '" + node->name + "' assigned to before declaration.");
+    }
+
+    // Resolve var type
+    node->type = idInfo->type;
+}
+
 void SemanticAnalysisVisitor::enterScopeFor(const ASTNode* ownerCtx) {
     // Init root
     if (current_ == nullptr) {
